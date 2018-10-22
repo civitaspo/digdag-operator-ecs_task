362772724f579967b669eb5b16930c78a2f2362a
Civitaspo <civitaspo@gmail.com>
2018-10-22 19:14:31 +0900

    Fix s3 file dl bug

--- src/main/scala/pro/civitaspo/digdag/plugin/ecs_task/result/EcsTaskResultOperator.scala
+++ src/main/scala/pro/civitaspo/digdag/plugin/ecs_task/result/EcsTaskResultOperator.scala
@@ -1,10 +1,15 @@
 package pro.civitaspo.digdag.plugin.ecs_task.result
+import java.io.File
+
 import com.amazonaws.services.s3.AmazonS3URI
 import com.amazonaws.services.s3.model.S3ObjectInputStream
+import com.amazonaws.services.s3.transfer.{Download, TransferManager, TransferManagerBuilder}
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.digdag.client.config.Config
 import io.digdag.spi.{OperatorContext, TaskResult, TemplateEngine}
 import pro.civitaspo.digdag.plugin.ecs_task.AbstractEcsTaskOperator
+
+import scala.io.Source
 
 class EcsTaskResultOperator(operatorName: String, context: OperatorContext, systemConfig: Config, templateEngine: TemplateEngine, objectMapper: ObjectMapper)
     extends AbstractEcsTaskOperator(operatorName, context, systemConfig, templateEngine, objectMapper) {
@@ -16,9 +21,15 @@ class EcsTaskResultOperator(operatorName: String, context: OperatorContext, syst
   val s3Uri: AmazonS3URI = AmazonS3URI(params.get("_command", classOf[String]))
 
   override def runTask(): TaskResult = {
-    val content: S3ObjectInputStream = aws.withS3(_.getObject(s3Uri.getBucket, s3Uri.getKey)).getObjectContent
-    val data: Config = objectMapper.readValue(content, classOf[Config])
-    import io.digdag.spi.TaskResult
+    val f: String = workspace.createTempFile("ecs_task.result", ".json")
+    aws.withS3 { s3 =>
+      val xfer: TransferManager = TransferManagerBuilder.standard().withS3Client(s3).build()
+      val download: Download = xfer.download(s3Uri.getBucket, s3Uri.getKey, new File(f))
+      download.waitForCompletion()
+    }
+    val content: String = Source.fromFile(f).getLines.mkString
+    val data: Config = cf.fromJsonString(content)
+
     TaskResult
       .defaultBuilder(cf)
       .subtaskConfig(data.getNestedOrGetEmpty("subtask_config"))
