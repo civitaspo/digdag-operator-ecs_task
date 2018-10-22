b47b7d4a734ac0053c45f942c53a50ee47d3c0ce
Civitaspo <civitaspo@gmail.com>
2018-10-22 17:55:21 +0900

    Impl EcsTaskResultOperator

--- src/main/scala/pro/civitaspo/digdag/plugin/ecs_task/result/EcsTaskResultOperator.scala
+++ src/main/scala/pro/civitaspo/digdag/plugin/ecs_task/result/EcsTaskResultOperator.scala
@@ -1,5 +1,6 @@
 package pro.civitaspo.digdag.plugin.ecs_task.result
 import com.amazonaws.services.s3.AmazonS3URI
+import com.amazonaws.services.s3.model.S3ObjectInputStream
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.digdag.client.config.Config
 import io.digdag.spi.{OperatorContext, TaskResult, TemplateEngine}
@@ -15,6 +16,14 @@ class EcsTaskResultOperator(operatorName: String, context: OperatorContext, syst
   val s3Uri: AmazonS3URI = AmazonS3URI(params.get("s3_uri", classOf[String]))
 
   override def runTask(): TaskResult = {
-    null
+    val content: S3ObjectInputStream = aws.withS3(_.getObject(s3Uri.getBucket, s3Uri.getKey)).getObjectContent
+    val data: Config = objectMapper.readValue(content, classOf[Config])
+    import io.digdag.spi.TaskResult
+    TaskResult
+      .defaultBuilder(cf)
+      .subtaskConfig(data.getNestedOrGetEmpty("subtask_config"))
+      .exportParams(data.getNestedOrGetEmpty("export_params"))
+      .storeParams(data.getNestedOrGetEmpty("store_params"))
+      .build
   }
 }
