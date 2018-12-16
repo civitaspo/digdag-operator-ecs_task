e21a1a237e6b837eb6101cfaa5a02e64b556847b
Mulyu <mulyu@hotmail.co.jp>
2018-12-16 07:38:24 +0000

    support polling strategy options.

---
+++ src/main/scala/pro/civitaspo/digdag/plugin/ecs_task/wait/ExponentialBackoffDelayStrategy.scala
@@ -0,0 +1,18 @@
+package pro.civitaspo.digdag.plugin.ecs_task.wait
+
+import com.amazonaws.waiters.PollingStrategy.DelayStrategy
+import com.amazonaws.waiters.PollingStrategyContext
+
+class ExponentialBackoffDelayStrategy(
+  interval: Int
+) extends DelayStrategy {
+
+  override def delayBeforeNextRetry(pollingStrategyContext: PollingStrategyContext): Unit = {
+    val nextDurationSec =
+      interval * Math.pow(2, pollingStrategyContext.getRetriesAttempted)
+
+    Thread.sleep(
+      nextDurationSec.toLong * 1000
+    )
+  }
+}
